9d197cbb5d129a3494e0bfbafeb3a8a40eb51378
James Puleo <james@jame.xyz>
2022-05-04 08:13:24 -0400

    Simplify semantics of `get_function_from_vtable_index`
    No reason to to cast to a `FlatPtr**`, just to later cast the
    twice dereference of that to a `Function`

--- src/Helpers.h
+++ src/Helpers.h
@@ -7,8 +7,7 @@ namespace Cosmo
 template<typename Function, typename T>
 inline Function get_function_from_vtable_index(const T* object_base_address, int index)
 {
-    auto* vtable = *reinterpret_cast<FlatPtr**>(const_cast<T*>(object_base_address));
-
-    return reinterpret_cast<Function>(vtable[index]);
+    auto** vtable = *reinterpret_cast<Function**>(const_cast<T*>(object_base_address));
+    return vtable[index];
 }
 }
