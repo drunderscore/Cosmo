79f1fd28944eed7b98ee39f37f42ebb7916ff4d6
James Puleo <james@jame.xyz>
2022-05-06 17:21:43 -0400

    Fix incorrect function signature for `CBaseEntity::IsPlayer` (again)
    Back in 193dd47, I said that we need to pass the `this` pointer through
    `ECX`. This is incorrect, and applies to MSVC, _not_ to GCC.
    `thiscall` for GCC is identical to `cdecl`, where the `this` pointer is
    passed as the first argument, so we will use that instead.

--- src/Types.h
+++ src/Types.h
@@ -13,7 +13,7 @@ class CBaseEntity : public IServerEntity
 public:
     inline bool IsPlayer() const
     {
-        return Cosmo::get_function_from_vtable_index<__attribute__((fastcall)) bool (*)(const CBaseEntity*)>(this,
-                                                                                                             81)(this);
+        return Cosmo::get_function_from_vtable_index<__attribute__((cdecl)) bool (*)(const CBaseEntity*)>(this,
+                                                                                                          81)(this);
     }
 };
