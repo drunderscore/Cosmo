35d9b4e6253a556a935a2018656000cc2aeedd9a
James Puleo <james@jame.xyz>
2022-05-08 09:55:41 -0400

    Allow a prototype `JS::Object` to be specified for `Entity`
    Instead of taking a `GlobalObject` and assuming our prototype is
    `EntityPrototype`, allow a constructor to state what their prototype is
    explicitly.
    
    `Entity` was made to not be `final` anymore, so it can actually be
    inherited.
    
    The `Entity::entity` getter was made to be templated, to allow returning
    of a derived `CBaseEntity` type. The default was made to be
    `CBaseEntity`, to keep existing usage.

--- src/Scripting/Entity.cpp
+++ src/Scripting/Entity.cpp
@@ -8,14 +8,11 @@
 
 namespace Cosmo::Scripting
 {
-Entity::Entity(GlobalObject& global_object, CBaseEntity* entity)
-    : Object(global_object.entity_prototype()), m_entity(entity)
-{
-}
+Entity::Entity(Object& prototype, CBaseEntity* entity) : Object(prototype), m_entity(entity) {}
 
 Entity* Entity::create(GlobalObject& global_object, CBaseEntity* entity)
 {
-    return global_object.heap().allocate<Entity>(global_object, global_object, entity);
+    return global_object.heap().allocate<Entity>(global_object, global_object.entity_prototype(), entity);
 }
 
 void Entity::initialize(JS::GlobalObject& global_object) { Object::initialize(global_object); }
--- src/Scripting/Entity.h
+++ src/Scripting/Entity.h
@@ -8,18 +8,23 @@
 
 namespace Cosmo::Scripting
 {
-class Entity final : public JS::Object
+class Entity : public JS::Object
 {
     JS_OBJECT(Entity, Object);
 
 public:
-    Entity(GlobalObject&, CBaseEntity*);
+    Entity(Object&, CBaseEntity*);
     void initialize(JS::GlobalObject&) override;
     ~Entity() override = default;
 
     static Entity* create(GlobalObject& global_object, CBaseEntity* entity);
 
-    CBaseEntity* entity() { return m_entity; }
+    template<typename T = CBaseEntity>
+        requires(IsBaseOf<CBaseEntity, T>)
+    T* entity()
+    {
+        return static_cast<T*>(m_entity);
+    }
 
 private:
     // FIXME: This is very much not correct! Once this entity is destroyed, this pointer is garbage.
